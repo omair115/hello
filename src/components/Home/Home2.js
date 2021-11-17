c68e4156e6f5a4d68969d30b511a9df3831d069c
Umair Anser <umair.anser115@gmail.com>
2021-11-17 12:27:47 +0500

    Update Home2.js
    added medium icon

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -7,7 +7,7 @@ import {
   AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import { FaLinkedinIn, FaMediumM } from "react-icons/fa";
 
 function Home2() {
   return (
@@ -95,6 +95,16 @@ function Home2() {
                   <FaLinkedinIn />
                 </a>
               </li>
+              <li className="social-icons">
+              <a
+                href="https://medium.com/@umairanser"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-colour  home-social-icons"
+              >
+                <FaMediumM />
+              </a>
+            </li>
             </ul>
           </Col>
         </Row>
